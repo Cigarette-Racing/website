0a6bb07cd7d9e6e5371ef78000ec5ac5b4764768
Ray Gesualdo <ray@raygesualdo.com>
2020-05-16 22:20:46 -0400

    wip

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+// import './layout.css'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -33,7 +33,7 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
+        <main className="bg-red-600">{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
