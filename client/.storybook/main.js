72d7c0f837350597296a7d77b270ada6345c6526
Ray Gesualdo <ray@raygesualdo.com>
2020-05-22 17:09:45 -0400

    storybook configuration

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,11 +1,35 @@
+const { resolve } = require('path')
+
+const pathToSvgs = resolve(__dirname, '../src/svgs')
+
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
   addons: [
     '@storybook/addon-actions',
-    '@storybook/addon-links',
     '@storybook/addon-backgrounds',
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport',
   ],
   webpackFinal: (config) => {
+    // Remove Storybook's default SVG handling
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test.test('.svg')
+    )
+    fileLoaderRule.exclude = pathToSvgs
+
+    // Use the SVGR loader for SVGs
+    config.module.rules.push({
+      test: /\.svg$/,
+      include: pathToSvgs,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    })
+
+    // Add postcss-loader for tailwind
     config.module.rules.push({
       test: /\.css$/,
       use: [
@@ -18,6 +42,8 @@ module.exports = {
         },
       ],
     })
+
+    // Handle Typescript files
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: [
@@ -30,6 +56,7 @@ module.exports = {
       ],
     })
     config.resolve.extensions.push('.ts', '.tsx')
+
     return config
   },
 }
