db8620cf9b5024f03168793613ca116f125704ec
Ray Gesualdo <ray@raygesualdo.com>
2020-05-22 23:25:54 -0400

    add ReturnLink atom

---
+++ client/stories/08-ReturnLink.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { withKnobs } from '@storybook/addon-knobs'
+import { ReturnLink } from '../src/ui-kit/return-link'
+
+export default {
+  title: 'Return Link',
+  component: ReturnLink,
+  decorators: [withKnobs],
+}
+
+export const Sandbox = () => {
+  return <ReturnLink theme="dark">Go Back</ReturnLink>
+}
