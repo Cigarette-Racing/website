18e7c54aad1215ef00a4c3cff2eec8f0066ddccd
Ray Gesualdo <ray@raygesualdo.com>
2020-05-22 22:22:15 -0400

    extract Theme type

---
+++ client/src/ui-kit/types.ts
@@ -0,0 +1,7 @@
+/**
+ * Theme to use.
+ *
+ * - light: A light background. Use dark text and overlays.
+ * - dark: A dark background. Use light text and overlays.
+ */
+export type Theme = 'light' | 'dark'
