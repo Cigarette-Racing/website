961afac07a040c8b915bf5053891b5265cf7a98b
Ray Gesualdo <ray@raygesualdo.com>
2020-05-22 23:23:49 -0400

    make icons more flexible
    - enable the `icon` SVGR option for SB's webpack config
    - pass font size and font color in stories

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -25,6 +25,12 @@ module.exports = {
       use: [
         {
           loader: '@svgr/webpack',
+          options: {
+            icon: true,
+            replaceAttrValues: {
+              '#fff': 'currentColor',
+            },
+          },
         },
       ],
     })
--- client/stories/05-Icons.stories.js
+++ client/stories/05-Icons.stories.js
@@ -15,9 +15,13 @@ import {
   ThreeDotsIcon,
   YoutubeIcon,
 } from '../src/svgs/icons'
+import { select } from '@storybook/addon-knobs'
+import clsx from 'clsx'
 
-const Container = ({ children }) => (
-  <div className="grid grid-cols-4 gap-12">{children}</div>
+const Container = ({ className, children }) => (
+  <div className={clsx('grid grid-cols-4 gap-12 text-2xl', className)}>
+    {children}
+  </div>
 )
 
 export default {
@@ -25,8 +29,17 @@ export default {
 }
 
 export const All = () => {
+  const colorClass = select(
+    'color',
+    {
+      White: 'text-white',
+      Black: 'text-black',
+      Red: 'text-red',
+    },
+    'text-white'
+  )
   return (
-    <Container>
+    <Container className={colorClass}>
       <ArrowIcon />
       <CaretIcon />
       <ExpandIcon />
@@ -45,17 +58,19 @@ export const All = () => {
   )
 }
 
-export const Arrow = () => <ArrowIcon />
-export const Caret = () => <CaretIcon />
-export const Expand = () => <ExpandIcon />
-export const ExternalLink = () => <ExternalLinkIcon />
-export const Facebook = () => <FacebookIcon />
-export const Instagram = () => <InstagramIcon />
-export const Mail = () => <MailIcon />
-export const Map = () => <MapIcon />
-export const Minus = () => <MinusIcon />
-export const Phone = () => <PhoneIcon />
-export const Play = () => <PlayIcon />
-export const Plus = () => <PlusIcon />
-export const ThreeDots = () => <ThreeDotsIcon />
-export const Youtube = () => <YoutubeIcon />
+export const Arrow = () => <ArrowIcon className="text-white text-2xl" />
+export const Caret = () => <CaretIcon className="text-white text-2xl" />
+export const Expand = () => <ExpandIcon className="text-white text-2xl" />
+export const ExternalLink = () => (
+  <ExternalLinkIcon className="text-white text-2xl" />
+)
+export const Facebook = () => <FacebookIcon className="text-white text-2xl" />
+export const Instagram = () => <InstagramIcon className="text-white text-2xl" />
+export const Mail = () => <MailIcon className="text-white text-2xl" />
+export const Map = () => <MapIcon className="text-white text-2xl" />
+export const Minus = () => <MinusIcon className="text-white text-2xl" />
+export const Phone = () => <PhoneIcon className="text-white text-2xl" />
+export const Play = () => <PlayIcon className="text-white text-2xl" />
+export const Plus = () => <PlusIcon className="text-white text-2xl" />
+export const ThreeDots = () => <ThreeDotsIcon className="text-white text-2xl" />
+export const Youtube = () => <YoutubeIcon className="text-white text-2xl" />
