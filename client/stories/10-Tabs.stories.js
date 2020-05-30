96fbf41041f72c95f50e865780c1ced360496a87
Ray Gesualdo <ray@raygesualdo.com>
2020-05-30 15:40:19 -0400

    add Tabs/Tab atoms

---
+++ client/stories/10-Tabs.stories.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { withKnobs } from '@storybook/addon-knobs'
+import { Tabs, Tab } from '../src/ui-kit/tabs'
+// @reach/router is a transitive dependency via Gatsby
+import {
+  createMemorySource,
+  createHistory,
+  LocationProvider,
+} from '@reach/router'
+
+const source = createMemorySource('/a')
+const history = createHistory(source)
+
+export default {
+  title: 'Tabs',
+  component: Tabs,
+  decorators: [withKnobs],
+}
+
+export const Sandbox = () => {
+  return (
+    <LocationProvider history={history}>
+      <Tabs>
+        <Tab to="/a">Category One</Tab>
+        <Tab to="/b">Category Two</Tab>
+        <Tab to="/c">Category Three</Tab>
+        <Tab to="/d">Category Four</Tab>
+      </Tabs>
+    </LocationProvider>
+  )
+}
