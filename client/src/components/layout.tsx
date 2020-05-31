6c98c8feb99131a95eb8b168cbb6356f2891571a
Ray Gesualdo <ray@raygesualdo.com>
2020-05-31 16:27:36 -0400

    wip on homepage

---
+++ client/src/components/layout.tsx
@@ -0,0 +1,35 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { Header } from '../molecules/header'
+import '../fonts/fonts.css'
+
+export const Layout: React.FC = ({ children }) => {
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
+
+  return (
+    <Fragment>
+      <Header />
+      <main className="bg-red-600">{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </Fragment>
+  )
+}
