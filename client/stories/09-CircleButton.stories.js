e5858bf5f943fdffc728bffa52661de8d0b904c2
Ray Gesualdo <ray@raygesualdo.com>
2020-05-31 13:12:16 -0400

    update CircleButton atom

--- client/stories/09-CircleButton.stories.js
+++ client/stories/09-CircleButton.stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { withKnobs, select } from '@storybook/addon-knobs'
 import { CircleButton } from '../src/ui-kit/circle-button'
 import { PlayIcon } from '../src/svgs/icons'
+import { action } from '@storybook/addon-actions'
 
 export default {
   title: 'Atoms/Circle Button',
@@ -14,6 +15,12 @@ export const Sandbox = () => {
   const theme = select('theme', ['light', 'dark'], 'dark')
   const size = select('size', ['sm', 'md', 'lg'], 'lg')
   return (
-    <CircleButton icon={PlayIcon} size={size} theme={theme} variant={variant} />
+    <CircleButton
+      icon={PlayIcon}
+      size={size}
+      theme={theme}
+      variant={variant}
+      onClick={action('Button click')}
+    />
   )
 }
