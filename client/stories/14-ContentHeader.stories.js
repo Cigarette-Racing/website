a09d9ad54452727f712cd34fd93ee2e4392683ac
Ray Gesualdo <ray@raygesualdo.com>
2020-05-31 14:27:41 -0400

    add ContentHeader atom

---
+++ client/stories/14-ContentHeader.stories.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { withKnobs, text } from '@storybook/addon-knobs'
+import { ContentHeader } from '../src/ui-kit/content-header'
+
+export default {
+  title: 'Atoms/ContentHeader',
+  component: ContentHeader,
+  decorators: [withKnobs],
+}
+
+export const Sandbox = () => {
+  const children = text('text', 'Tirranna 59 AMG Edition')
+  const subtitle = text('subtitle', '')
+  return <ContentHeader subtitle={subtitle}>{children}</ContentHeader>
+}
