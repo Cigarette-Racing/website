d82915f4805e0024ac2b1441e1872abc11d299e3
Ray Gesualdo <ray@raygesualdo.com>
2020-05-31 16:34:00 -0400

    temporarily surpress TS errors

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,11 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import {Layout} from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { Layout } from '../components/layout'
+// @ts-ignore
+import Image from '../components/image'
+// @ts-ignore
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
