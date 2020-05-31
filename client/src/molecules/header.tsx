0e4aa707e57dcb09bb7d14c6ae0bd4756f30564c
Ray Gesualdo <ray@raygesualdo.com>
2020-05-31 16:27:36 -0400

    wip on homepage

---
+++ client/src/molecules/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import clsx from 'clsx'
+import logo from '../images/logo-white.svg'
+import { Typography } from '../ui-kit/typography'
+
+export interface HeaderProps {}
+
+export const Header = ({}: HeaderProps) => {
+  return (
+    <div className="bg-black bg-opacity-50 flex justify-between items-center text-white h-20">
+      <Typography variant="e3">Hamburger</Typography>
+      <img src={logo} className="h-16 py-2" />
+      <Typography variant="e3">Contact</Typography>
+    </div>
+  )
+}
