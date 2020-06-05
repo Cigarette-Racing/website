ddb87a5c109deda2a524058ccbebc9f01823fee5
Ray Gesualdo <ray@raygesualdo.com>
2020-06-05 12:31:43 -0400

    add SocialLink atom

---
+++ client/src/atoms/social-link.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import clsx from 'clsx'
+import { Typography } from './typography'
+
+export interface SocialLinkProps
+  extends React.LinkHTMLAttributes<HTMLAnchorElement> {
+  icon: React.ElementType
+  text: string
+}
+
+export const SocialLink = ({
+  className,
+  icon: Icon,
+  text,
+  ...props
+}: SocialLinkProps) => {
+  return (
+    <a
+      {...props}
+      className={clsx(
+        'text-white flex flex-col sm:flex-row items-center space-y-6 sm:space-y-0 sm:space-x-6',
+        className
+      )}
+    >
+      <Icon className="w-6 h-6" />
+      <Typography variant="e3">{text}</Typography>
+    </a>
+  )
+}
