29b36e1d4db254a230b7fb7fec1076fee7eff0d1
Ray Gesualdo <ray@raygesualdo.com>
2020-06-05 12:31:43 -0400

    add SocialLink atom

---
+++ client/stories/17-SocialLink.stories.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { SocialLink } from '../src/atoms/social-link'
+import { FacebookIcon, InstagramIcon, YoutubeIcon } from '../src/svgs/icons'
+
+export default {
+  title: 'Atoms/Social Link',
+  component: SocialLink,
+}
+
+const linkVariants = [
+  { icon: FacebookIcon, text: 'Facebook', href: '#' },
+  { icon: InstagramIcon, text: 'Instagram', href: '#' },
+  { icon: YoutubeIcon, text: 'Youtube', href: '#' },
+]
+
+export const Sandbox = () => {
+  return (
+    <div className="space-y-8">
+      {linkVariants.map((linkVariant) => {
+        return <SocialLink {...linkVariant} />
+      })}
+    </div>
+  )
+}
