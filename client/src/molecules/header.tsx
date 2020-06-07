ed45a065cf96bfeabdf63e59e70e6e02d5da2a7e
Ray Gesualdo <ray@raygesualdo.com>
2020-06-06 21:54:43 -0400

    header molecule WIP

--- client/src/molecules/header.tsx
+++ client/src/molecules/header.tsx
@@ -9,10 +9,18 @@ export const Header = ({}: HeaderProps) => {
   // @ts-ignore
   const src: string = logo
   return (
-    <div className="bg-black bg-opacity-50 flex justify-between items-center text-white h-20">
-      <Typography variant="e3">Hamburger</Typography>
-      <img src={src} className="h-16 py-2" />
-      <Typography variant="e3">Contact</Typography>
+    <div
+      className="absolute top-0 left-0 w-full z-10 text-white h-20"
+      style={{
+        background:
+          'linear-gradient(180deg, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0) 100%)',
+      }}
+    >
+      <div className="max-w-8xl mx-auto h-full px-4 flex justify-between items-center">
+        <Typography variant="e3">Hamburger</Typography>
+        <img src={src} className="h-16 py-2" />
+        <Typography variant="e3">Contact</Typography>
+      </div>
     </div>
   )
 }
