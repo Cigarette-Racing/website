46135803ca39202ae643593f92eb8324442904df
Ray Gesualdo <ray@raygesualdo.com>
2020-06-08 21:30:46 -0400

    refactor ReturnLink atom

--- client/src/atoms/return-link.tsx
+++ client/src/atoms/return-link.tsx
@@ -1,27 +1,36 @@
-import React, { HTMLProps } from 'react'
+import React from 'react'
 import clsx from 'clsx'
 import { Typography } from './typography'
 import { ArrowIcon } from '../svgs/icons'
 import { Theme } from './types'
 
-export interface ReturnLinkProps {
+export interface ReturnLinkProps
+  extends React.HTMLAttributes<HTMLButtonElement> {
   /** Light or dark theme */
-  theme: Theme
+  theme?: Theme
 }
 
-export const ReturnLink: React.FC<
-  ReturnLinkProps & HTMLProps<HTMLAnchorElement>
-> = ({ children, theme, ...rest }) => {
+export const ReturnLink: React.FC<ReturnLinkProps> = ({
+  children,
+  theme,
+  className,
+  ...rest
+}) => {
   return (
-    <a
+    <button
+      type="button"
       {...rest}
-      className={clsx('flex items-center', {
-        'text-white': theme === 'dark',
-        'text-black': theme === 'light',
-      })}
+      className={clsx(
+        'flex items-center',
+        {
+          'text-white': theme === 'dark',
+          'text-black': theme === 'light',
+        },
+        className
+      )}
     >
       <ArrowIcon className="mr-4 text-xl text-red transform rotate-180" />
       <Typography variant="e3">{children}</Typography>
-    </a>
+    </button>
   )
 }
