cc6261eb01f190fc2bcb15b104a129c5f74c0a10
Patrick Hoydar <pshoydar@gmail.com>
2020-07-04 11:10:40 -0400

    move form to its own component

---
+++ client/src/molecules/inquiry/inquiry-form.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Form } from 'react-final-form'
+
+const InquiryForm = ({ children, onSubmit }: any) => (
+  <form
+    method="post"
+    netlify-honeypot="bot-field"
+    data-netlify="true"
+    name="inquiry-form"
+    onSubmit={onSubmit}
+  >
+    <input type="hidden" name="bot-field" />
+    <input type="hidden" name="form-name" value="inquiry-form" />
+
+    {children}
+
+    <button type="submit">Submit</button>
+  </form>
+)
+
+export default InquiryForm
