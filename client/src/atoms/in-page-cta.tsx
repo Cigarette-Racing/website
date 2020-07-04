a0218282fa9302ef8b8aa0f1929edda31100a097
Ray Gesualdo <ray@raygesualdo.com>
2020-07-03 20:37:45 -0400

    finish contact page

--- client/src/atoms/in-page-cta.tsx
+++ client/src/atoms/in-page-cta.tsx
@@ -4,6 +4,8 @@ import { Typography } from './typography'
 import { Theme } from '../types/shared'
 
 export interface InPageCtaProps {
+  /** Make the button a link */
+  link?: true
   /** Visual variant to use */
   variant?: 'primary' | 'secondary'
   /** Light or dark theme */
@@ -11,28 +13,49 @@ export interface InPageCtaProps {
 }
 
 const baseClasses =
-  'h-10 px-4 sm:px-10 border rounded-full transition-colors duration-150 ease-in-out'
+  'inline-flex items-center h-10 px-4 sm:px-10 border rounded-full transition-colors duration-150 ease-in-out'
 const primaryClasses =
   'bg-white text-black border-white hover:bg-red hover:border-red hover:text-white'
 const secondaryClasses = 'border-red hover:bg-red'
 
 export const InPageCta: React.FC<
-  InPageCtaProps & React.HTMLAttributes<HTMLButtonElement>
+  InPageCtaProps &
+    (
+      | React.HTMLAttributes<HTMLButtonElement>
+      | React.AnchorHTMLAttributes<HTMLLinkElement>
+    )
 > = ({ variant = 'primary', theme, children, ...rest }) => {
-  return (
-    <button
-      {...rest}
-      type="button"
-      className={clsx(baseClasses, {
-        [primaryClasses]: variant === 'primary',
-        [secondaryClasses]: variant === 'secondary',
-        'text-white': variant === 'secondary' && theme === 'dark',
-        'text-black': variant === 'secondary' && theme === 'light',
-      })}
-    >
-      <Typography variant="e2" as="span">
-        {children}
-      </Typography>
-    </button>
-  )
+  const classNames = clsx(baseClasses, {
+    [primaryClasses]: variant === 'primary',
+    [secondaryClasses]: variant === 'secondary',
+    'bg-black text-white': variant === 'secondary' && theme === 'dark',
+    'text-black': variant === 'secondary' && theme === 'light',
+  })
+  if (isLink(rest)) {
+    return (
+      <a {...rest} className={classNames}>
+        <Typography variant="e2" as="span">
+          {children}
+        </Typography>
+      </a>
+    )
+  }
+  if (isButton(rest)) {
+    return (
+      <button {...rest} type="button" className={classNames}>
+        <Typography variant="e2" as="span">
+          {children}
+        </Typography>
+      </button>
+    )
+  }
+  return null
+}
+
+function isLink(rest: any): rest is HTMLLinkElement {
+  return !!rest.link
+}
+
+function isButton(rest: any): rest is HTMLButtonElement {
+  return !rest.link
 }
