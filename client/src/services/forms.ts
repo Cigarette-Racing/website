f3f06fae905ddc8c7facd35d4b70a9154384ed48
Ray Gesualdo <ray@raygesualdo.com>
2020-07-15 21:07:04 -0400

    write custom hook for subscribing to form state

--- client/src/services/forms.ts
+++ client/src/services/forms.ts
@@ -1,3 +1,8 @@
+import string from '*.svg'
+import { useForm } from 'react-final-form'
+import { useLayoutEffect, useEffect, useState } from 'react'
+import { FormState, AnyObject } from 'final-form'
+
 const id = (x: any) => x
 
 type OnSubmitCreator = (mapper?: (x: any) => any) => (values: any) => void
@@ -15,4 +20,22 @@ export const onSubmitCreator: OnSubmitCreator = (mapper = id) => (values) => {
       ...submissionValues,
     }),
   })
+}
+
+export function useFormState({
+  subscription,
+}: {
+  subscription: Record<string, boolean>
+}) {
+  const form = useForm('useFormState__CUSTOM')
+  const [state, setState] = useState(() => form.getState())
+  useEffect(
+    () =>
+      form.subscribe((newState) => {
+        // @ts-ignore
+        setState(newState)
+      }, subscription),
+    []
+  )
+  return state
 }
