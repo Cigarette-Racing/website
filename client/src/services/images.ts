187f9ffcd9ca8680db931b25806bfc7d400a9d9d
Ray Gesualdo <ray@raygesualdo.com>
2020-07-18 18:01:18 -0400

    refactor image caching code

--- client/src/services/images.ts
+++ client/src/services/images.ts
@@ -1,15 +1,16 @@
 function createImageCacher() {
   const cache: Record<string, boolean> = {}
+  const createImage = (url: string): void => {
+    if (cache[url]) return
+
+    const img = new Image()
+    img.src = url
+
+    cache[url] = true
+  }
 
   return (urls: string[]) => {
-    urls.forEach((url) => {
-      if (cache[url]) return
-
-      const img = new Image()
-      img.src = url
-
-      cache[url] = true
-    })
+    urls.forEach(createImage)
   }
 }
 
