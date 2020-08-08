332a4383b18b13aa184e6112f4ac392108f27f63
Patrick Hoydar <pshoydar@gmail.com>
2020-08-08 15:53:43 -0400

    carousels

--- client/src/templates/boat.tsx
+++ client/src/templates/boat.tsx
@@ -88,15 +88,20 @@ const extractFlexibleSectionsFromCraft = (boatEntry: any) => {
     oneColumnImageTextBlock: 'one-column-image-text',
     twoColumnImageTextBlock: 'two-column-image-text',
     twoColumnImagesBlock: 'two-column-images',
-    threeColumnImagesBlock: '',
-    sliderBlock: '',
-    carouselBlock: '',
-    fullWidthCarouselBlock: '',
+    threeColumnImagesBlock: 'three-column-images',
+    slider: 'slider',
+    carousel: 'carousel',
+    fullWidthCarousel: 'full-width-carousel',
   }
 
   return boatEntry.flexibleSections.map((section: any) => {
-    const blocks = section.blocks.map((block) => {
+    section.blocks.map((block) => {
+      block.source = 'craft'
       block.type = blockTypes[block.typeHandle]
+
+      if (block.typeHandle === 'carousel' && block.fullWidth) {
+        block.type = 'full-width-carousel'
+      }
     })
 
     return {
@@ -142,6 +147,20 @@ const extractOrderDataFromCraft = (boatEntry: any) => {
     title: boatEntry.orderSectionTitle || 'Order Today',
     media: boatEntry.orderSectionBackground[0]?.url,
   }
+}
+
+const createCarouselItems = (items: any) => {
+  return items.map((item) => {
+    return {
+      content: {
+        copy: item.singleMedia.textBlock?.[0].copy,
+        header: item.singleMedia.textBlock?.[0].header,
+      },
+      media: {
+        image: item.singleMedia?.[0].image?.[0].url,
+      },
+    }
+  })
 }
 
 const BoatTemplate = (props: PageProps<GatsbyTypes.BoatPageQuery>) => {
@@ -241,12 +260,14 @@ const BoatTemplate = (props: PageProps<GatsbyTypes.BoatPageQuery>) => {
               blocks.map((block, index) => {
                 if (isTwoColumnImageTextBlock(block)) {
                   return (
-                    <TwoColumnImageTextBlockComponent key={index} {...block} />
+                    <div>TWO COLUMN IMAGE TEXT BLOCK</div>
+                    // <TwoColumnImageTextBlockComponent key={index} {...block} />
                   )
                 }
                 if (isOneColumnTextBlock(block)) {
                   if (block.textBlock) {
                     block.copy = block.textBlock[0].copy
+                    block.header = block.textBlock[0].header
                   }
 
                   return (
@@ -267,21 +288,27 @@ const BoatTemplate = (props: PageProps<GatsbyTypes.BoatPageQuery>) => {
 
                   return <OneColumnImageTextBlockComponent {...block} />
                 }
-                // if (isCarouselBlock(block)) {
-                //   return <Carousel key={index} {...block} />
-                // }
-                // if (isSliderBlock(block)) {
-                //   return <Slider key={index} {...block} />
-                // }
-                // if (isThreeColumnImagesBlock(block)) {
-                //   return (
-                //     <ThreeUpImageBlock
-                //       key={index}
-                //       className="mb-32"
-                //       images={block.images}
-                //     />
-                //   )
-                // }
+                if (isCarouselBlock(block)) {
+                  if (block?.source === 'craft') {
+                    const items = createCarouselItems(block.children)
+                    block.items = items
+                  }
+
+                  return <Carousel key={index} {...block} />
+                }
+                if (isSliderBlock(block)) {
+                  return <div>SLIDER BLOCK</div>
+                  // return <Slider key={index} {...block} />
+                }
+                if (isThreeColumnImagesBlock(block)) {
+                  return (
+                    <ThreeUpImageBlock
+                      key={index}
+                      className="mb-32"
+                      images={block.images}
+                    />
+                  )
+                }
                 if (isTwoColumnImagesBlock(block)) {
                   return (
                     <TwoUpImageBlock
@@ -291,9 +318,16 @@ const BoatTemplate = (props: PageProps<GatsbyTypes.BoatPageQuery>) => {
                     />
                   )
                 }
-                // if (isFullWidthCarouselBlock(block)) {
-                //   return <FullWidthCarousel key={index} {...block} />
-                // }
+                if (isFullWidthCarouselBlock(block)) {
+                  console.log('full width CAROUSEL')
+
+                  if (block?.source === 'craft') {
+                    const items = createCarouselItems(block.children)
+                    block.items = items
+                  }
+
+                  return <FullWidthCarousel key={index} {...block} />
+                }
                 return null
               })}
             {moreDetails && (
@@ -404,6 +438,23 @@ export const query = graphql`
                   textBlock {
                     ... on CraftAPI_textBlock_BlockType {
                       copy
+                      header
+                    }
+                  }
+                }
+                ... on CraftAPI_flexibleSections_oneColumnImageTextBlock_BlockType {
+                  textBlock {
+                    ... on CraftAPI_textBlock_BlockType {
+                      copy
+                    }
+                  }
+                  singleMedia {
+                    ... on CraftAPI_singleMedia_BlockType {
+                      image {
+                        ... on CraftAPI_s3_Asset {
+                          url
+                        }
+                      }
                     }
                   }
                 }
@@ -422,18 +473,32 @@ export const query = graphql`
                     }
                   }
                 }
-                ... on CraftAPI_flexibleSections_oneColumnImageTextBlock_BlockType {
+                ... on CraftAPI_flexibleSections_twoColumnImageTextBlock_BlockType {
                   id
-                  textBlock {
-                    ... on CraftAPI_textBlock_BlockType {
-                      copy
-                    }
-                  }
-                  singleMedia {
-                    ... on CraftAPI_singleMedia_BlockType {
-                      image {
-                        ... on CraftAPI_s3_Asset {
-                          url
+                }
+                ... on CraftAPI_flexibleSections_threeColumnImagesBlock_BlockType {
+                  id
+                }
+                ... on CraftAPI_flexibleSections_sliderBlock_BlockType {
+                  id
+                }
+                ... on CraftAPI_flexibleSections_carousel_BlockType {
+                  fullWidth
+                  children {
+                    typeHandle
+                    ... on CraftAPI_flexibleSections_oneColumnImageTextBlock_BlockType {
+                      textBlock {
+                        ... on CraftAPI_textBlock_BlockType {
+                          copy
+                        }
+                      }
+                      singleMedia {
+                        ... on CraftAPI_singleMedia_BlockType {
+                          image {
+                            ... on CraftAPI_s3_Asset {
+                              url
+                            }
+                          }
                         }
                       }
                     }
