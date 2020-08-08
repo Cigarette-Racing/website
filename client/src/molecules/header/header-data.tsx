d0d4471352b78829c59fe64eb2f67482ad1ba605
Ray Gesualdo <ray@raygesualdo.com>
2020-08-08 15:15:48 -0400

    wip on header menu

--- client/src/molecules/header/header-data.tsx
+++ client/src/molecules/header/header-data.tsx
@@ -1,5 +1,6 @@
 import { graphql, useStaticQuery } from 'gatsby'
-import { findHeroSection } from '../../types/boat'
+import orderBy from 'lodash/orderBy'
+import { findHeroSection, Stat } from '../../types/boat'
 
 export const useBoatsQuery = () => {
   const data = useStaticQuery<GatsbyTypes.HeaderBoatsMenuQuery>(
@@ -34,6 +35,38 @@ export const useBoatsQuery = () => {
             }
           }
         }
+        craftAPI {
+          entries(type: "boats") {
+            __typename
+            ... on CraftAPI_boats_boats_Entry {
+              title
+              slug
+              boatNameLong
+              stats: boatStats {
+                ... on CraftAPI_boatStats_stat_BlockType {
+                  label: statLabel
+                  percentage: statPercentage
+                  text: statText
+                }
+              }
+              boatMetadata {
+                ... on CraftAPI_boatMetadata_BlockType {
+                  menuName
+                  menuSortOrder
+                }
+              }
+              backgroundMedia: singleMedia {
+                ... on CraftAPI_singleMedia_BlockType {
+                  image {
+                    ... on CraftAPI_s3_Asset {
+                      url
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
       }
     `
   )
@@ -41,6 +74,31 @@ export const useBoatsQuery = () => {
 }
 
 const extractBoats = (data: GatsbyTypes.HeaderBoatsMenuQuery) => {
+  const menuItems = data.craftAPI.entries?.map((entry) => {
+    if (entry?.__typename !== 'CraftAPI_boats_boats_Entry') {
+      return
+    }
+    return {
+      boatName: entry?.title!,
+      menuName: entry?.boatMetadata?.[0]?.menuName! || entry?.boatNameLong!,
+      menuOrder: (entry?.boatMetadata?.[0]?.menuSortOrder as Number) || 0,
+      slug: entry?.slug!,
+      stats: entry?.stats?.map(
+        (stat) =>
+          ({
+            label: stat?.label!,
+            percentage: stat?.percentage! as Number,
+            text: stat?.text!,
+          } as Stat)
+      )!,
+      backgroundMedia: {
+        image: {
+          publicUrl: entry?.backgroundMedia?.[0]?.image?.[0]?.url!,
+        },
+      },
+    }
+  })
+  return orderBy(menuItems, ['menuOrder'])
   return data.boats.nodes.map((node) => {
     const heroSection = findHeroSection(node.sections!)!
     return {
