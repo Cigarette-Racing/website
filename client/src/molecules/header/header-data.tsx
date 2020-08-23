ab7d611072e4c287c9d7fd82c3b637302b0724d4
Ray Gesualdo <ray@raygesualdo.com>
2020-08-23 00:54:08 -0400

    get smoother scrolling working in the boat header menu

--- client/src/molecules/header/header-data.tsx
+++ client/src/molecules/header/header-data.tsx
@@ -52,35 +52,52 @@ export type HeaderBoatMenuCategories =
   | 'highPerformance'
   | 'none'
 
+export type HeaderBoat = {
+  boatName: string
+  menuName: string
+  menuOrder: Number
+  menuCategory: string
+  slug: string
+  stats: Stat[]
+  backgroundMedia: {
+    image: {
+      publicUrl: string
+    }
+    alt: GatsbyTypes.Maybe<string>
+  }
+}
+
 const extractBoats = (data: GatsbyTypes.HeaderBoatsMenuQuery) => {
   // Use .flatMap to convince Typescript that there will never be
   // `undefined` values in the array... 🤦‍♂️
-  const menuItems = data.craftAPI.entries?.flatMap((entry) => {
-    if (entry?.__typename !== 'CraftAPI_boats_boats_Entry') {
-      return []
-    }
-    return {
-      boatName: entry?.title!,
-      menuName: entry?.boatMetadata?.[0]?.menuName! || entry?.boatNameLong!,
-      menuOrder: (entry?.boatMetadata?.[0]?.menuSortOrder as Number) || 0,
-      menuCategory: entry?.boatMetadata?.[0]?.menuCategory || 'none',
-      slug: entry?.slug!,
-      stats: entry?.stats?.map(
-        (stat) =>
-          ({
-            label: stat?.label!,
-            percentage: stat?.percentage! as Number,
-            text: stat?.text!,
-          } as Stat)
-      )!,
-      backgroundMedia: {
-        image: {
-          publicUrl: entry?.backgroundMedia?.[0]?.image?.[0]?.url!,
+  const menuItems: HeaderBoat[] | undefined = data.craftAPI.entries?.flatMap(
+    (entry) => {
+      if (entry?.__typename !== 'CraftAPI_boats_boats_Entry') {
+        return []
+      }
+      return {
+        boatName: entry?.title!,
+        menuName: entry?.boatMetadata?.[0]?.menuName! || entry?.boatNameLong!,
+        menuOrder: (entry?.boatMetadata?.[0]?.menuSortOrder as Number) || 0,
+        menuCategory: entry?.boatMetadata?.[0]?.menuCategory || 'none',
+        slug: entry?.slug!,
+        stats: entry?.stats?.map(
+          (stat) =>
+            ({
+              label: stat?.label!,
+              percentage: stat?.percentage! as Number,
+              text: stat?.text!,
+            } as Stat)
+        )!,
+        backgroundMedia: {
+          image: {
+            publicUrl: entry?.backgroundMedia?.[0]?.image?.[0]?.url!,
+          },
+          alt: entry?.backgroundMedia?.[0]?.alt,
         },
-        alt: entry?.backgroundMedia?.[0]?.alt,
-      },
+      }
     }
-  })
+  )
 
   const preparedMenuItems = pipe(
     groupBy('menuCategory'),
