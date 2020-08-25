528936e95d9a5c0e82ecd9c77434c1ab5a4bb37f
Ray Gesualdo <ray@raygesualdo.com>
2020-08-24 21:48:30 -0400

    add google analytics

--- client/gatsby-config.ts
+++ client/gatsby-config.ts
@@ -68,6 +68,12 @@ export const plugins = [
       icon: `src/images/favicon.png`, // This path is relative to the root of the site.
     },
   },
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: process.env.GOOGLE_ANALYTICS_ID,
+    },
+  },
   // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
   // `gatsby-plugin-offline`,
