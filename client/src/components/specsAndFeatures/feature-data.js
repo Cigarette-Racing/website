33d4a0843f2f3b930b2506a9464d7bbe2d238dc2
Patrick Hoydar <pshoydar@gmail.com>
2021-02-04 12:57:51 -0500

    add anotehr seciton for placeholder

--- client/src/components/specsAndFeatures/feature-data.js
+++ client/src/components/specsAndFeatures/feature-data.js
@@ -63,6 +63,38 @@ const features = [
       },
     ],
   },
+  {
+    name: 'Details',
+    data: [
+      {
+        title: `BOW, GUNWALE & HULL SIDES`,
+        details: [
+          'Wide profile rub rail with stainless chafe profile',
+          'Stainless steel continuous grab rail runs the length of the boat',
+          'Large, inboard swinging, single-piece, dive door on starboard side with hidden fold-out diving ladder and handrail',
+          '10 stainless steel cleats',
+        ],
+      },
+      {
+        title: `BOW AREA`,
+        details: [
+          'U shaped seating',
+          'Dual, aft facing, benches sitting two each',
+          '4 stainless steel cupholders around C-shaped sofas',
+          'Weathertight storage boxes underneath each seat',
+        ],
+      },
+      {
+        title: `More Features`,
+        details: [
+          'Wide profile rub rail with stainless chafe profile',
+          'Stainless steel continuous grab rail runs the length of the boat',
+          'Large, inboard swinging, single-piece, dive door on starboard side with hidden fold-out diving ladder and handrail',
+          '10 stainless steel cleats',
+        ],
+      },
+    ],
+  },
 ]
 
 export default features
