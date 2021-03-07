4b824b7bf9a70e5d75cc19383b4e6693e9d3df05
Patrick Hoydar <pshoydar@gmail.com>
2021-03-07 14:20:53 -0500

    set body selector in useEffect fix

--- client/src/templates/article.components.tsx
+++ client/src/templates/article.components.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import clsx from 'clsx'
 import styled from 'styled-components'
 import { PageProps, graphql, Link } from 'gatsby'
@@ -49,92 +49,90 @@ export const Categories = ({
   )
 }
 
+const bodyElement = document.body
+
 export const DropdownNav = ({ placeholder, options, onChange }) => {
-  let menuPortalTarget = null
-
-  useEffect(() => {
-    menuPortalTarget = document.body
-  }, [])
-
   return (
-    <Select
-      className="mb-12"
-      options={options}
-      placeholder={placeholder}
-      onChange={onChange}
-      menuPortalTarget={menuPortalTarget || ''}
-      components={{
-        DropdownIndicator: (props) => (
-          <components.DropdownIndicator {...props}>
-            <CaretDownIcon />
-          </components.DropdownIndicator>
-        ),
-        ValueContainer: (props) => (
-          <components.ValueContainer {...props}>
-            <Typography variant="e2">{props.children}</Typography>
-          </components.ValueContainer>
-        ),
-        Option: (props) => (
-          <components.Option
-            {...props}
-            className="py-3 text-center bg-transparent screen text-white hover:bg-gray-1 hover:text-gray-6"
-          >
-            <Typography variant="e1">{props.children}</Typography>
-          </components.Option>
-        ),
-      }}
-      styles={{
-        placeholder: (base) => ({
-          ...base,
-          color: '#fff',
-        }),
-        control: (base) => ({
-          ...base,
-          background: 'rgba(255,255,255,0.1)',
-          border: '1px solid #fff',
-          borderRadius: '40px',
-        }),
-        clearIndicator: () => ({
-          display: 'none',
-        }),
-        indicatorSeparator: () => ({
-          display: 'none',
-        }),
-        indicatorsContainer: () => ({
-          order: 1,
-          padding: '0 5px 0 10px',
-        }),
-        dropdownIndicator: (base) => ({
-          ...base,
-          fontSize: '1rem',
-          color: '#d12026',
-        }),
-        valueContainer: (base) => ({
-          ...base,
-          order: 2,
-          padding: '10px 0',
-          color: '#fff',
-        }),
-        singleValue: (base) => ({
-          ...base,
-          color: '#fff',
-        }),
-        menu: (base) => ({
-          ...base,
-          background: '#242424',
-        }),
-        option: (base, state) => ({
-          ...base,
-          color: '#fff',
-          cursor: 'pointer',
-          backgroundColor: state.isSelected ? '#d12026' : 'transparent',
-          '&:active': {
-            backgroundColor: '#d12026',
+    <div>
+      <Select
+        className="mb-12"
+        options={options}
+        placeholder={placeholder}
+        onChange={onChange}
+        menuPortalTarget={bodyElement}
+        components={{
+          DropdownIndicator: (props) => (
+            <components.DropdownIndicator {...props}>
+              <CaretDownIcon />
+            </components.DropdownIndicator>
+          ),
+          ValueContainer: (props) => (
+            <components.ValueContainer {...props}>
+              <Typography variant="e2">{props.children}</Typography>
+            </components.ValueContainer>
+          ),
+          Option: (props) => (
+            <components.Option
+              {...props}
+              className="py-3 text-center bg-transparent screen text-white hover:bg-gray-1 hover:text-gray-6"
+            >
+              <Typography variant="e1">{props.children}</Typography>
+            </components.Option>
+          ),
+        }}
+        styles={{
+          placeholder: (base) => ({
+            ...base,
             color: '#fff',
-          },
-        }),
-      }}
-    />
+          }),
+          control: (base) => ({
+            ...base,
+            background: 'rgba(255,255,255,0.1)',
+            border: '1px solid #fff',
+            borderRadius: '40px',
+          }),
+          clearIndicator: () => ({
+            display: 'none',
+          }),
+          indicatorSeparator: () => ({
+            display: 'none',
+          }),
+          indicatorsContainer: () => ({
+            order: 1,
+            padding: '0 5px 0 10px',
+          }),
+          dropdownIndicator: (base) => ({
+            ...base,
+            fontSize: '1rem',
+            color: '#d12026',
+          }),
+          valueContainer: (base) => ({
+            ...base,
+            order: 2,
+            padding: '10px 0',
+            color: '#fff',
+          }),
+          singleValue: (base) => ({
+            ...base,
+            color: '#fff',
+          }),
+          menu: (base) => ({
+            ...base,
+            background: '#242424',
+          }),
+          option: (base, state) => ({
+            ...base,
+            color: '#fff',
+            cursor: 'pointer',
+            backgroundColor: state.isSelected ? '#d12026' : 'transparent',
+            '&:active': {
+              backgroundColor: '#d12026',
+              color: '#fff',
+            },
+          }),
+        }}
+      />
+    </div>
   )
 }
 
